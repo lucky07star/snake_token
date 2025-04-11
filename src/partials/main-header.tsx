704758cf244c19d684f7a6eb527e51f11ac73c5e
lucky07star <luckystar07180223@gmail.com>
2025-04-12 02:55:24 +0900

    fixed: main header padding style

--- src/partials/main-header.tsx
+++ src/partials/main-header.tsx
@@ -5,7 +5,7 @@ function MainHeader() {
     return (
         <>
             <div style={{ height: '10vh' }}></div>
-            <div className="main-header d-flex fixed-top justify-content-between align-items-center border-bottom border-5 border-black bg-black w-100 p-2 shadow-bottom" style={{ height: '10vh' }}>
+            <div className="main-header d-flex fixed-top justify-content-between align-items-center border-bottom border-5 border-black bg-black w-100 pt-2 px-2 px-md-4 px-lg-4 shadow-bottom" style={{ height: '10vh' }}>
                 <div className="d-flex justify-content-start align-items-center gap-2">
                     <IconSmallLogo className="fs-1" />
                     <span className="fs-1 fs-lg-7 fs-xl-7 text-white main-header-title">SNAKE.AI</span>
