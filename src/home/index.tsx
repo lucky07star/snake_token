64dfc087b1ed5abd0c5acdd0c47102631ef64fd6
lucky07star <luckystar07180223@gmail.com>
2025-04-17 19:36:42 +0000

    Update index.tsx

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,5 +1,6 @@
 
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Transaction, Connection, sendAndConfirmRawTransaction } from '@solana/web3.js';
 import { Buffer } from "buffer";
 import { useNotify } from "../hooks/useNotify";
@@ -22,7 +23,7 @@ import { ReactComponent as IconColoredLogo } from '../svgs/logo-colored.svg';
 import { ReactComponent as IconArrowUp } from '../svgs/arrow-up.svg';
 import { ReactComponent as IconArrowDown } from '../svgs/arrow-down.svg';
 
-import getMe from "../features/users/apis/getMe";
+import getProfile from "../features/users/apis/getProfile";
 import getRewards from "../features/users/apis/getRewards";
 import postClaimTx from "../features/wallet/apis/postClaimTx";
 import postWalletAddress from "../features/wallet/apis/postWalletAddress";
@@ -43,19 +44,20 @@ function Home() {
 
     const { notify } = useNotify();
     const { walletAvailable, connect, publicKey, disconnect } = usePhantom();
+    const { value } = useParams();
 
     const [userInfo, setUserInfo] = useState<USER | null>(null);
     const [availableRewards, setAvailableRewards] = useState<REWARDS[] | []>([]);
     const [meReward, setMeReward] = useState<any>([]);
     const [tx, setTx] = useState<string>('')
 
-    const getMeAPI = getMe();
+    const getProfileAPI = getProfile();
     const getRewardsAPI = getRewards();
     const postClaimTxAPI = postClaimTx();
     const postWalletAddressAPI = postWalletAddress();
 
     useEffect(() => {
-        getMeAPI().then(res => {
+        getProfileAPI().then(res => {
             setUserInfo(res.data);
         });
         getRewardsAPI({
@@ -64,6 +66,11 @@ function Home() {
             setMeReward(data.data);
         });
     }, [])
+
+    useEffect(() => {
+        if(Number(value) === 1) 
+            handleRewards();
+    }, [value])
 
     useEffect(() => {
         const run = async () => {
@@ -200,13 +207,13 @@ function Home() {
             {/* Tab Bars */}
             <div className="d-flex justify-content-between align-items-end gap-3 pt-md-3 mb-2 row-reverse">
                 <div className={`border-bottom-5 border-bottom-dashed py-3 mobile-tab ${mobileState ? (selectedTab === 'menu' ? '' : 'border-bottom-gray text-gray-61') : ''}`} style={{ width: '130px' }} onClick={() => tabClicked('menu')}>
-                    <div className="fs-2 text-center text-green-960" style={{ lineHeight: 'normal' }}>MENU</div>
+                    <div className="fs-2 text-center" style={{ lineHeight: 'normal' }}>MENU</div>
                 </div>
                 <div className={`border-bottom-5 border-bottom-dashed py-3 mobile-tab mobile-tab-fixed ${mobileState ? (selectedTab === 'dashboard' ? '' : 'border-bottom-gray text-gray-61') : ''}`} onClick={() => tabClicked('dashboard')}>
-                    <div className="fs-2 text-center text-green-960" style={{ lineHeight: 'normal' }}>{dashboardText}</div>
+                    <div className="fs-2 text-center" style={{ lineHeight: 'normal' }}>{dashboardText}</div>
                 </div>
                 <div className="border-bottom-5 border-bottom-dashed py-3 mobile-tab mobile-hidden" style={{ width: '50%' }}>
-                    <div className="fs-2 text-center text-green-960" style={{ lineHeight: 'normal' }}>Mining Progress</div>
+                    <div className="fs-2 text-center" style={{ lineHeight: 'normal' }}>Mining Progress</div>
                 </div>
             </div>
             {/* Main Components */}
@@ -218,15 +225,15 @@ function Home() {
                             <div className={`d-flex justify-content-center align-items-center flex-wrap ${!mobileState ? 'h-md-75' : ''}`}>
                                 <div className={`w-100 d-flex justify-content-around align-items-center ${!mobileState ? '' : 'border-5 border-dashed mb-4 px-4'} cursor-pointer`} onClick={() => { setPageState('home'); setSelectedTab('dashboard'); setShowMiningProgress(false); setDashboardText('Dashboard'); }}>
                                     {pageState === 'home' || pageState === 'claim-rewards' ? <IconHomeSelected className="main-menu-icon" /> : <IconHome className="main-menu-icon" />}
-                                    {mobileState ? <div className="m-0 fs-1 text-center text-green-960" style={{ lineHeight: 'normal' }}>Home</div> : ''}
+                                    {mobileState ? <div className="m-0 fs-1 text-center" style={{ lineHeight: 'normal' }}>Home</div> : ''}
                                 </div>
                                 <div className={`w-100 d-flex justify-content-around align-items-center ${!mobileState ? '' : 'border-5 border-dashed mb-4 px-4'} cursor-pointer`} onClick={() => { setPageState('mining'); setSelectedTab('dashboard'); setShowMiningProgress(false); }}>
                                     {pageState === 'mining' ? <IconMiningSelected className="main-menu-icon" /> : <IconMining className="main-menu-icon" />}
-                                    {mobileState ? <div className="m-0 fs-1 text-center text-green-960" style={{ lineHeight: 'normal' }}>Mining</div> : ''}
+                                    {mobileState ? <div className="m-0 fs-1 text-center" style={{ lineHeight: 'normal' }}>Mining</div> : ''}
                                 </div>
                                 <div className={`w-100 d-flex justify-content-around align-items-center ${!mobileState ? '' : 'border-5 border-dashed mb-4 px-4'} cursor-pointer`} onClick={() => { setPageState('claim'); setSelectedTab('dashboard'); setShowMiningProgress(false); }}>
                                     {pageState === 'claim' ? <IconClaimSelected className="main-menu-icon" /> : <IconClaim className="main-menu-icon" />}
-                                    {mobileState ? <div className="m-0 fs-1 text-center text-green-960" style={{ lineHeight: 'normal' }}>Claim</div> : ''}
+                                    {mobileState ? <div className="m-0 fs-1 text-center" style={{ lineHeight: 'normal' }}>Claim</div> : ''}
                                 </div>
                             </div>
                         </div> : ''
@@ -240,37 +247,34 @@ function Home() {
                                     return <>
                                         <div className="d-flex align-items-center w-100 border border-dashed border-5 p-1">
                                             <IconColoredLogo className="mx-1" />
-                                            <span className="mx-2 fs-6 fs-xl-13 fs-xxl-15 text-green-960">Hello,</span>
+                                            <span className="mx-2 fs-6 fs-xl-13 fs-xxl-15 font-gotham">Hello,</span>
                                             <span className="fs-6 fs-xl-13 fs-xxl-14" style={{ color: '#D7263D' }}>{userInfo?.twitter_username}</span>
                                         </div>
                                         <div className="py-md-3 p-1 border-bottom-5 border-bottom-dashed item-stretch item-dash" style={{ minHeight: 'calc(100vh - 240px)' }}>
                                             <div className="py-2 d-flex justify-content-between align-items-center">
-                                                <span className="fs-6 fs-lg-11 fs-xl-12 fw-bolder text-green-960" style={{ lineHeight: 'normal' }}>REWARD BALANCE:</span>
-                                                <span className="py-2 px-2 border border-4 border-black rounded-4 bg-light-green-950 text-center fs-6 fs-lg-12 fs-xl-13">123,456 SNAKES</span>
+                                                <span className="fs-6 fs-lg-11 fs-xl-12 fw-bolder" style={{ lineHeight: 'normal' }}>REWARD BALANCE:</span>
+                                                <span className="py-2 px-2 border border-4 border-black rounded-4 bg-light-green-950 text-center fs-6 fs-lg-12 fs-xl-13">{userInfo?.reward_balance} SNAKES</span>
                                             </div>
                                             <div className="separator py-2"><div className="separator-3 separator-dashed separator-black"></div></div>
                                             <div className="w-100 py-2">
-                                                <div className="w-100 d-flex justify-content-center mb-1">
-                                                    <button className="border border-4 border-black bg-gray-300 w-75 py-3 fs-6 fs-xl-11 fs-xxl-13 w-100 text-green-960" style={{ lineHeight: 'normal' }} onClick={handleRewards}>Your Rewards</button>
-                                                </div>
                                                 <div className="w-100 d-flex justify-content-center">
-                                                    <button className="border border-4 border-black bg-light-green-950 w-75 py-3 fs-6 fs-xl-11 fs-xxl-13 w-100 text-green-960" style={{ lineHeight: 'normal' }}>Start Mining</button>
+                                                    <button className="border border-4 border-black bg-gray-300 w-75 py-3 fs-6 fs-xl-11 fs-xxl-13 w-100" style={{ lineHeight: 'normal' }} onClick={handleRewards}>Your Rewards</button>
                                                 </div>
                                             </div>
                                             <div className="separator py-2"><div className="separator-3 separator-dashed separator-black"></div></div>
-                                            <span className="fs-6 fs-lg-11 fs-xl-12 fw-bolder text-green-960">ENGAGEMENT METRICS:</span>
+                                            <span className="fs-6 fs-lg-11 fs-xl-12 fw-bolder">ENGAGEMENT METRICS:</span>
                                             <div className="separator py-2"><div className="separator-3 separator-dashed separator-black"></div></div>
                                             <div className="w-100 d-flex justify-content-center align-items-between gap-1">
                                                 <div className="w-100 border border-light-green-950 border-3 py-4 px-2 text-center bg-green-950 rounded-2">
-                                                    <div className="fs-4 fs-xl-9 fs-xxl-8 fw-bolder text-light-green-950">42</div>
+                                                    <div className="fs-4 fs-xl-9 fs-xxl-8 fw-bolder text-light-green-950">{userInfo?.tweets}</div>
                                                     <div className="fs-8 fs-xl-13 fs-xxl-14 text-green-300">Tweets</div>
                                                 </div>
                                                 <div className="w-100 border border-light-green-950 border-3 py-4 px-2 text-center bg-green-950 rounded-2">
-                                                    <div className="fs-4 fs-xl-9 fs-xxl-8 fw-bolder text-light-green-950">189</div>
+                                                    <div className="fs-4 fs-xl-9 fs-xxl-8 fw-bolder text-light-green-950">{userInfo?.likes}</div>
                                                     <div className="fs-8 fs-xl-13 fs-xxl-14 text-green-300">Likes</div>
                                                 </div>
                                                 <div className="w-100 border border-light-green-950 border-3 py-4 px-2 text-center bg-green-950 rounded-2">
-                                                    <div className="fs-4 fs-xl-9 fs-xxl-8 fw-bolder text-light-green-950">87</div>
+                                                    <div className="fs-4 fs-xl-9 fs-xxl-8 fw-bolder text-light-green-950">{userInfo?.replies}</div>
                                                     <div className="fs-8 fs-xl-13 fs-xxl-14 text-green-300">Replies</div>
                                                 </div>
                                             </div>
@@ -311,7 +315,7 @@ function Home() {
                                     <div className="w-100 border-5 border-dashed item-progress-accodion px-3 py-1 cursor-pointer">
                                         <div className={`d-flex justify-content-between align-items-center`}>
                                             <div className="m-0 text-center">
-                                                <span className="text-green-960" style={{ lineHeight: 'normal' }}>Reward History</span>
+                                                <span style={{ lineHeight: 'normal' }}>Reward History</span>
                                             </div>
                                             {showRewardHistory ? <IconArrowDown style={{ width: '27px' }} onClick={() => setShowRewardHistory(false)} /> : <IconArrowUp style={{ width: '27px' }} onClick={() => setShowRewardHistory(true)} />}
                                         </div>
@@ -327,7 +331,7 @@ function Home() {
                                 return mobileState ? <>
                                     <div className="w-100 border-5 border-dashed d-flex justify-content-between align-items-center item-progress-accodion px-3 py-1 cursor-pointer">
                                         <div className="m-0 text-center">
-                                            <span className="text-green-960" style={{ lineHeight: 'normal' }}>Mining Progress </span>
+                                            <span style={{ lineHeight: 'normal' }}>Mining Progress </span>
                                             <span className="text-light-green-950" style={{ lineHeight: 'normal' }}>50%</span>
                                         </div>
                                         <IconArrowUp style={{ width: '27px' }} onClick={() => setShowMiningProgress(true)} />
