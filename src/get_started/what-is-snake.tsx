70c9876ee89fd0fa13d93dc57b662c38b7552a0a
lucky07star <luckystar07180223@gmail.com>
2025-04-27 22:43:15 +0900

    fix: feedback on get started page

--- src/get_started/what-is-snake.tsx
+++ src/get_started/what-is-snake.tsx
@@ -9,8 +9,8 @@ function WhatIsSnakePage() {
                     <IconRocket style={{ transform: 'rotate(30deg)', width: '30vh', height: '30vh' }} />
                 </div>
                 <div className="text-center col-md-7">
-                    <div className="fs-4 fs-lg-5 fs-xl-5 font-silkscreen-bold mb-4 text-green-960" style={{ lineHeight: 'normal' }}>What is SNAKE.AI?</div>
-                    <span className="fs-6 fs-lg-10 fs-xl-10 text-center w-75" style={{ lineHeight: 'normal' }}><span className="font-silkscreen-bold">SNAKE.AI</span> is the world’s first meme-fuelled, deflationary token ecosystem built on Solana, inspired by the legendary Nokia Snake game. Tweet, mine tokens, and be part of a viral cultural moment.
+                    <div className="fs-4 fs-lg-5 fs-xl-5 font-silkscreen-bold mb-4 text-green-960" style={{ lineHeight: 'normal' }}>What is playSNAKE.AI?</div>
+                    <span className="fs-6 fs-lg-10 fs-xl-10 text-center w-75" style={{ lineHeight: 'normal' }}><span className="font-silkscreen-bold">playSNAKE.AI</span> is the world’s first meme-fuelled, deflationary token ecosystem built on Solana, inspired by the legendary Nokia Snake game. Tweet, mine tokens, and be part of a viral cultural moment.
                     </span>
                 </div>
             </div>
